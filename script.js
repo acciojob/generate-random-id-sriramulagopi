be3cc99e505d60889b2f88685e7714634565895d
sriramulagopi <129825285+sriramulagopi@users.noreply.github.com>
2023-12-18 11:33:12 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,15 @@
 function makeid(l) {
   // write your code here
+    let str="";
+    while(str.length<l){
+        let ran = Math.floor(Math.random()*123);
+        if ((ran>47 && ran<58) || (ran>64 && ran<91) || (ran>96 && ran<123)){
+            str+=String.fromCharCode(ran);
+        }
+    }
+	return str;
 }
-
 // Do not change the code below.
 const l = prompt("Enter a number.");
 alert(makeid(l));
+
